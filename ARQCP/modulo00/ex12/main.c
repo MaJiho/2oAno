8c5d957a3c6466ae86879015dc2ad3b7794c8347
MaJiho <44770252+MaJiho@users.noreply.github.com>
2019-09-22 18:41:12 +0100

    ARQCP
    ex12 (changes)

--- ARQCP/modulo00/ex12/main.c
+++ ARQCP/modulo00/ex12/main.c
@@ -3,13 +3,13 @@
 
 int main() {
 	char x[] = "I will master ARQCP";
-	printf("Size =%u\n", sizeof(x));
-	printf("Size =%u\n", size_string_wrong (x));
-	printf("Size =%u\n", size_string_correct(x));
+	printf("Size =%u\n", sizeof(x)); //Tamanho da string com o \0 no fim
+	printf("Size =%u\n", size_string_wrong (x)); //Tamanho do endereço
+	printf("Size =%u\n", size_string_correct(x)); //Tamanho da string sem o \0
 	
 	char y[25] = "I will master ARQCP";
-	printf("\n Size =%u\n", sizeof(y));
-	printf("Size =%u\n", size_string_wrong (y));
-	printf("Size =%u\n", size_string_correct(y));
+	printf("\n Size =%u\n", sizeof(y));//Tamanho da string ditado (25)
+	printf("Size =%u\n", size_string_wrong (y));//Tamanho do endereço
+	printf("Size =%u\n", size_string_correct(y));//Tamanho da string
 	return 0;
 }
